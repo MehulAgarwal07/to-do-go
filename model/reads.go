157a6ca02c7abff3fb4d54183f0eced8dc0f66b4
Mehul Agarwal <mehulagarwal@pop-os.localdomain>
2020-06-20 14:17:08 +0530

    Reading from Database

---
+++ model/reads.go
@@ -0,0 +1,19 @@
+package model
+
+import (
+	"github.com/MehulAgarwal07/to-do-go/views"
+)
+
+func ReadAll() ([]views.PostRequest, error) {
+	rows, err := con.Query("SELECT * FROM TODO")
+	if err != nil {
+		return nil, err
+	}
+	todos := []views.PostRequest{}
+	for rows.Next() {
+		data := views.PostRequest{}
+		rows.Scan(&data.Name, &data.Todo)
+		todos = append(todos, data)
+	}
+	return todos, nil
+}
